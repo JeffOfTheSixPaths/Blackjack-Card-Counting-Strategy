efcc2cea36643c6acbba00cb29fea370dbe22acd
Tendo <71673094+JeffOfTheSixPaths@users.noreply.github.com>
2023-10-26 08:05:00 -0500

    Betting changes

--- Game.py
+++ Game.py
@@ -2,7 +2,8 @@ from random import shuffle
 class Deck:
     def __init__(self):
         self.unshuffled_deck = ['AC', 'AS', 'AD', 'AH', '2C', '2S', '2D', '2H', '3C', '3S', '3D', '3H', '4C', '4S', '4D', '4H', '5C', '5S', '5D', '5H', '6C', '6S', '6D', '6H', '7C', '7S', '7D', '7H', '8C', '8S', '8D', '8H', '9C', '9S', '9D', '9H','TC', 'TS', 'TD', 'TH', 'JC', 'JS', 'JD', 'JH', 'QC', 'QS', 'QD', 'QH', 'KC', 'KS', 'KD', 'KH']
-        self.deck = shuffle(self.unshuffled_deck)
+        self.deck = self.unshuffled_deck[:]
+        shuffle(self.deck)
 
 class Game:
     def __init__(self,player, deck):
@@ -23,17 +24,24 @@ class Game:
         return card_value
     def lose(self,bet):
         self.player.money -= bet
+        
     
     def win(self,bet):
         self.player.money += bet
-
+        #
     def play_shoe(self,player, deck):
-        bet = player.make_bet()
-        if bet > player.money: bet = player.money
-        if player.money == 0: 
-            raise ValueError('no money!!')
-
+        # reset player deck
+        player.known_deck = deck.unshuffled_deck
+        deck = deck.deck
+        
         while len(deck) > 10:
+            bet = player.make_bet()
+            if bet > player.money: bet = player.money
+            if bet < 0: bet = 0
+            if player.money <= 0: 
+                player.default += player.money - 300
+                player.money = 300
+                raise ValueError('no')
             #make a new hand
             dealer_cards = [deck[0]]
             player_cards = [deck[1], deck[2]]
@@ -41,7 +49,7 @@ class Game:
 
             #removing cards from possible cards
             for card in new_cards:
-                player.known_deck.remove(card)
+                player.known_deck.remove(str(card))
                 player.count += player.card_count[card[0]]
                 
 
@@ -86,16 +94,23 @@ class Game:
                         dealer_count += self.get_card_value(card, dealer_count)
                     break
             # now both players have had their turn 
-            if lost: self.lose(bet)
+            if lost: 
+                self.lose(bet)
+                print(f"{player_count} {dealer_count} lost with a bet of {bet} -- bal: {self.player.money}")
             else:
                 if dealer_count > 21:
                     self.win(bet)
+                    print(f"{player_count} {dealer_count} won with a bet of {bet} -- bal: {self.player.money}")
                 elif player_count > dealer_count:
                     self.win(bet)
-                else:
+                    print(f"{player_count} {dealer_count} won with a bet of {bet} -- bal: {self.player.money}")
+                elif player_count< dealer_count:
                     self.lose(bet)
+                    print(f"{player_count} {dealer_count} lost with a bet of {bet} -- bal: {self.player.money}")
+                else:
+                    print(f"{player_count} {dealer_count} tied with a bet of {bet} -- bal: {self.player.money}")
                 
-                
+        player.money += player.default      
         return player.money
 
 
--- Player.py
+++ Player.py
@@ -1,4 +1,4 @@
-import numpy as numpy
+import numpy as np
 import random
 from Game import *
 
@@ -11,8 +11,10 @@ class Player:
         self.money = 300
         self.count = 0
         self.known_deck = ['AC', 'AS', 'AD', 'AH', '2C', '2S', '2D', '2H', '3C', '3S', '3D', '3H', '4C', '4S', '4D', '4H', '5C', '5S', '5D', '5H', '6C', '6S', '6D', '6H', '7C', '7S', '7D', '7H', '8C', '8S', '8D', '8H', '9C', '9S', '9D', '9H','TC', 'TS', 'TD', 'TH', 'JC', 'JS', 'JD', 'JH', 'QC', 'QS', 'QD', 'QH', 'KC', 'KS', 'KD', 'KH']
+        self.default = 0
 
     def bet_func(self,count, bet_function):
+        if count < 4: return 1
         mon = 20
         for pair in bet_function:
             mon += pair[0] * ( count ** pair[1] )
@@ -21,7 +23,7 @@ class Player:
     def make_bet(self):
         return self.bet_func(self.count, self.bet_function)
 
-hilo = {
+cc = {
     '2':1,
     '3':1,
     '4':1,
@@ -37,11 +39,30 @@ hilo = {
     'A':-1,
     
 }
-bet = [[1, 1], [2,2]]
-pt = 1
+bet = [[1, 1], [2,2], [3,2]]
+pt = .56
 
-p = Player(hilo, bet, pt)
+p = Player(cc, bet, pt)
 deck = Deck()
 g = Game(p, deck.deck)
-#print(deck.unshuffled_deck)
-print(g.play_shoe(p, deck.unshuffled_deck))
+m = 0
+for i in range(10000):
+    m+=g.play_shoe(p, Deck())
+print(m)
+print(p.default)
+n_players = 10000
+l_rate = 1e-4
+players = []
+
+for n in range(n_players):
+    # starting with random card counting values
+    for key in cc:
+        cc[key] = np.random.uniform(-1,1)
+    
+    #changing how the bet's made
+    for i,e in enumerate(bet):
+        for j in range(2):
+            bet[i][j] = np.random.normal(1, 0.1)
+
+# 
+#print(cc)
